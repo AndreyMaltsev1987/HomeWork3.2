a08b90e204c7742f9be4e0827b467b5ad8ecf6cc
Andrey <andryuxa16@yandex.ru>
2023-11-19 22:30:49 +0300

    add commit Task02.java

---
+++ src/Task02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        System.out.println("Задание 2");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int n = sc.nextInt();
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+        }
+        System.out.println(result);
+
+
+
+    }
+}
